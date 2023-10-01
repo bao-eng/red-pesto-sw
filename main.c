1e358ea315b4e387be09e1f40b82679dcdc1f845
Andrey Basov <dev.basov@gmail.com>
2023-10-02 00:56:51 +0100

    bq25619e charger demo

--- main.c
+++ main.c
@@ -1,157 +1,112 @@
-// /**
-//  * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
-//  *
-//  * SPDX-License-Identifier: BSD-3-Clause
-//  */
-
-// #include "pico/stdlib.h"
-
-// int main() {
-//     const uint LED_PIN = 2;
-//     gpio_init(LED_PIN);
-//     gpio_set_dir(LED_PIN, GPIO_OUT);
-//     while (true) {
-//         gpio_put(LED_PIN, 1);
-//         sleep_ms(250);
-//         gpio_put(LED_PIN, 0);
-//         sleep_ms(250);
-//     }
-// }
-
-
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-// Fade an LED between low and high brightness. An interrupt handler updates
-// the PWM slice's output level each time the counter wraps.
-
-// #include "pico/stdlib.h"
-// #include <stdio.h>
-// #include "pico/time.h"
-// #include "hardware/irq.h"
-// #include "hardware/pwm.h"
-
-// #define LED_PIN 13
-
-// int main() {
-//     // Tell the LED pin that the PWM is in charge of its value.
-//     gpio_set_function(LED_PIN, GPIO_FUNC_PWM);
-//     // Figure out which slice we just connected to the LED pin
-//     uint slice_num = pwm_gpio_to_slice_num(LED_PIN);
-
-//     // Get some sensible defaults for the slice configuration. By default, the
-//     // counter is allowed to wrap over its maximum range (0 to 2**16-1)
-//     pwm_config config = pwm_get_default_config();
-//     // Set divider, reduces counter clock to sysclock/this value
-//     (&config, 4.f);
-//     // Load the configuration into our PWM slice, and set it running.
-//     pwm_init(slice_num, &config, true);
-//     pwm_set_gpio_level(LED_PIN, 0);
-
-//     // Everything after this point happens in the PWM interrupt handler, so we
-//     // can twiddle our thumbs
-//     bool go_up = true;
-//     uint16_t duty = 0;
-//     while (1){
-//         if(go_up){
-//             duty++;
-//             if(duty >=50){
-//                 go_up=false;
-//             }
-//         }else{
-//             duty--;
-//             if(duty == 0){
-//                 go_up=true;
-//             }
-//         }
-//         pwm_set_gpio_level(LED_PIN, duty*duty);
-//         sleep_ms(100);
-//         // tight_loop_contents();
-//     }
-// }
-
-
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-// Sweep through all 7-bit I2C addresses, to see if any slaves are present on
-// the I2C bus. Print out a table that looks like this:
-//
-// I2C Bus Scan
-//   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
-// 0
-// 1       @
-// 2
-// 3             @
-// 4
-// 5
-// 6
-// 7
-//
-// E.g. if slave addresses 0x12 and 0x34 were acknowledged.
-
 #include <stdio.h>
-#include "pico/stdlib.h"
-#include "pico/binary_info.h"
+
+#include "bq256xx_charger.h"
+#include "hardware/adc.h"
 #include "hardware/i2c.h"
+#include "pico/binary_info.h"
+#include "pico/stdlib.h"
 
 #define I2C_INST i2c0
 #define I2C_SDA_PIN 20
 #define I2C_SCL_PIN 21
+#define CHRG_EN_PIN 10
+#define STATUS_PIN 16
+#define BQ_INT_PIN 11
 
-// // #define I2C_INST i2c1
-// // #define I2C_SDA_PIN 14
-// // #define I2C_SCL_PIN 15
+#define CC1_PIN 26
+#define CC2_PIN 27
+#define CC1_ADC 0
+#define CC2_ADC 1
+#define VBAT_PIN 28
+#define VBAT_ADC 2
 
+#define BTN_PIN 9
 
-// I2C reserves some addresses for special purposes. We exclude these from the scan.
-// These are any addresses of the form 000 0xxx or 111 1xxx
-bool reserved_addr(uint8_t addr) {
-    return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
+// https://hackaday.com/2023/01/04/all-about-usb-c-resistors-and-emarkers/
+#define VRD_USB_MIN 0.25f
+#define VRD_USB_MAX 0.61f
+#define VRD_15_MIN 0.7f
+#define VRD_15_MAX 1.16f
+#define VRD_30_MIN 1.31f
+#define VRD_30_MAX 2.04f
+#define VRA_MAX 0.15f
+
+struct bq256xx_device bq25619e = {
+    .i2c = I2C_INST,
+    .dev_addr = 0x6A,
+};
+
+void gpio_callback(uint gpio, uint32_t events) {
+  if (events == GPIO_IRQ_EDGE_RISE && gpio == BQ_INT_PIN) {
+    busy_wait_ms(100);  // wait for the CC voltage to set
+    const float conversion_factor = 3.3f / (1 << 12);
+    adc_select_input(CC1_ADC);
+    float cc1 = adc_read() * conversion_factor;
+    adc_select_input(CC2_ADC);
+    float cc2 = adc_read() * conversion_factor;
+    float cc = cc1 > cc2 ? cc1 : cc2;
+    if ((cc > VRD_USB_MIN) && (cc < VRD_USB_MAX)) {
+      printf("vRd-USB (500mA)\n");
+      bq256xx_set_input_curr_lim(&bq25619e, 500000);
+    } else if ((cc > VRD_15_MIN) && (cc < VRD_15_MAX)) {
+      printf("vRd-1.5\n");
+      bq256xx_set_input_curr_lim(&bq25619e, 1500000);
+    } else if ((cc > VRD_30_MIN) && (cc < VRD_30_MAX)) {
+      printf("vRd-3.0\n");
+      bq256xx_set_input_curr_lim(&bq25619e, 3000000);
+    } else if (cc < VRA_MAX) {
+      printf("vRa (disconnected)\n");
+      bq256xx_set_input_curr_lim(&bq25619e, 500000);
+    }
+  }
+  if (events == GPIO_IRQ_EDGE_FALL && gpio == BTN_PIN) {
+    printf("BUTTON_INT!!!\n");
+  }
 }
 
 int main() {
-    // Enable UART so we can print status output
-    stdio_init_all();
-    // This example will use I2C0 on the default SDA and SCL pins (GP4, GP5 on a Pico)
-    i2c_init(I2C_INST, 100 * 1000);
-    gpio_set_function(I2C_SDA_PIN, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA_PIN);
-    gpio_pull_up(I2C_SCL_PIN);
-    // Make the I2C pins available to picotool
-    bi_decl(bi_2pins_with_func(I2C_SDA_PIN, I2C_SCL_PIN, GPIO_FUNC_I2C));
+  stdio_init_all();
 
-    printf("\nI2C Bus Scan\n");
-    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+  adc_init();
+  adc_gpio_init(CC1_PIN);
+  adc_gpio_init(CC2_PIN);
 
-    for (int addr = 0; addr < (1 << 7); ++addr) {
-        if (addr % 16 == 0) {
-            printf("%02x ", addr);
-        }
+  adc_gpio_init(VBAT_PIN);
 
-        // Perform a 1-byte dummy read from the probe address. If a slave
-        // acknowledges this address, the function returns the number of bytes
-        // transferred. If the address byte is ignored, the function returns
-        // -1.
+  gpio_init(STATUS_PIN);
+  gpio_set_dir(STATUS_PIN, GPIO_OUT);
 
-        // Skip over any reserved addresses.
-        int ret;
-        uint8_t rxdata;
-        if (reserved_addr(addr))
-            ret = PICO_ERROR_GENERIC;
-        else
-            ret = i2c_read_blocking(I2C_INST, addr, &rxdata, 1, false);
+  gpio_init(BQ_INT_PIN);
+  gpio_set_dir(BQ_INT_PIN, GPIO_IN);
+  gpio_pull_up(BQ_INT_PIN);
+  gpio_set_irq_enabled_with_callback(BQ_INT_PIN, GPIO_IRQ_EDGE_RISE, true,
+                                     &gpio_callback);
 
-        printf(ret < 0 ? "." : "@");
-        printf(addr % 16 == 15 ? "\n" : "  ");
-    }
-    printf("Done.\n");
-    return 0;
+  gpio_set_dir(CHRG_EN_PIN, GPIO_OUT);
+  gpio_put(CHRG_EN_PIN, false);
+
+  gpio_set_dir(BTN_PIN, GPIO_IN);
+  gpio_set_irq_enabled_with_callback(BTN_PIN, GPIO_IRQ_EDGE_FALL, true,
+                                     &gpio_callback);
+
+  i2c_init(I2C_INST, 100 * 1000);
+  gpio_set_function(I2C_SDA_PIN, GPIO_FUNC_I2C);
+  gpio_set_function(I2C_SCL_PIN, GPIO_FUNC_I2C);
+  gpio_pull_up(I2C_SDA_PIN);
+  gpio_pull_up(I2C_SCL_PIN);
+
+  bq256xx_charger_reset(&bq25619e);
+  bq256xx_hw_init(&bq25619e);
+
+  struct bq256xx_state state1;
+  bq256xx_get_state(&bq25619e, &state1);
+
+  while (1) {
+    const float conversion_factor = 6.4f / (1 << 12);
+    adc_select_input(VBAT_ADC);
+    printf("VBAT: %.2f V\n", adc_read() * conversion_factor);
+    sleep_ms(1000);
+  }
+
+  return 0;
 }
