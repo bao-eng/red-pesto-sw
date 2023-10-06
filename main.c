9972b0fa799bd79acd305b094e452ddec1c03076
Andrey Basov <dev.basov@gmail.com>
2023-10-06 03:34:02 +0100

    LIS2DH12 driver integrated

--- main.c
+++ main.c
@@ -12,102 +12,23 @@
 
 #include "lis2dh12-pid/lis2dh12_reg.h"
 
-/* Example code to talk to a bme280 humidity/temperature/pressure sensor.
-
-   NOTE: Ensure the device is capable of being driven at 3.3v NOT 5v. The Pico
-   GPIO (and therefore SPI) cannot be used at 5v.
-
-   You will need to use a level shifter on the SPI lines if you want to run the
-   board at 5v.
-
-   Connections on Raspberry Pi Pico board and a generic bme280 board, other
-   boards may vary.
-
-   GPIO 16 (pin 21) MISO/spi0_rx-> SDO/SDO on bme280 board
-   GPIO 17 (pin 22) Chip select -> CSB/!CS on bme280 board
-   GPIO 18 (pin 24) SCK/spi0_sclk -> SCL/SCK on bme280 board
-   GPIO 19 (pin 25) MOSI/spi0_tx -> SDA/SDI on bme280 board
-   3.3v (pin 36) -> VCC on bme280 board
-   GND (pin 38)  -> GND on bme280 board
-
-   Note: SPI devices can have a number of different naming schemes for pins. See
-   the Wikipedia page at https://en.wikipedia.org/wiki/Serial_Peripheral_Interface
-   for variations.
-
-   This code uses a bunch of register definitions, and some compensation code derived
-   from the Bosch datasheet which can be found here.
-   https://www.bosch-sensortec.com/media/boschsensortec/downloads/datasheets/bst-bme280-ds002.pdf
-*/
-
 #define READ_BIT 0x80
 
-int32_t t_fine;
+#define    BOOT_TIME            5 //ms
 
-uint16_t dig_T1;
-int16_t dig_T2, dig_T3;
-uint16_t dig_P1;
-int16_t dig_P2, dig_P3, dig_P4, dig_P5, dig_P6, dig_P7, dig_P8, dig_P9;
-uint8_t dig_H1, dig_H3;
-int8_t dig_H6;
-int16_t dig_H2, dig_H4, dig_H5;
+/* Self test limits converted from 10bit right-aligned to 16bit left-aligned. */
+#define    MIN_ST_LIMIT_LSb     17*64
+#define    MAX_ST_LIMIT_LSb    360*64
 
-/* The following compensation functions are required to convert from the raw ADC
-data from the chip to something usable. Each chip has a different set of
-compensation parameters stored on the chip at point of manufacture, which are
-read from the chip at startup and used in these routines.
-*/
-int32_t compensate_temp(int32_t adc_T) {
-    int32_t var1, var2, T;
-    var1 = ((((adc_T >> 3) - ((int32_t) dig_T1 << 1))) * ((int32_t) dig_T2)) >> 11;
-    var2 = (((((adc_T >> 4) - ((int32_t) dig_T1)) * ((adc_T >> 4) - ((int32_t) dig_T1))) >> 12) * ((int32_t) dig_T3))
-            >> 14;
+/* Private variables ---------------------------------------------------------*/
+static int16_t data_raw_acceleration[3];
+static float acceleration_st_mg[3];
+static float acceleration_mg[3];
+static uint8_t tx_buffer[1000];
+static float test_val_mg[3];
+static float max_st_limit_mg;
+static float min_st_limit_mg;
 
-    t_fine = var1 + var2;
-    T = (t_fine * 5 + 128) >> 8;
-    return T;
-}
-
-uint32_t compensate_pressure(int32_t adc_P) {
-    int32_t var1, var2;
-    uint32_t p;
-    var1 = (((int32_t) t_fine) >> 1) - (int32_t) 64000;
-    var2 = (((var1 >> 2) * (var1 >> 2)) >> 11) * ((int32_t) dig_P6);
-    var2 = var2 + ((var1 * ((int32_t) dig_P5)) << 1);
-    var2 = (var2 >> 2) + (((int32_t) dig_P4) << 16);
-    var1 = (((dig_P3 * (((var1 >> 2) * (var1 >> 2)) >> 13)) >> 3) + ((((int32_t) dig_P2) * var1) >> 1)) >> 18;
-    var1 = ((((32768 + var1)) * ((int32_t) dig_P1)) >> 15);
-    if (var1 == 0)
-        return 0;
-
-    p = (((uint32_t) (((int32_t) 1048576) - adc_P) - (var2 >> 12))) * 3125;
-    if (p < 0x80000000)
-        p = (p << 1) / ((uint32_t) var1);
-    else
-        p = (p / (uint32_t) var1) * 2;
-
-    var1 = (((int32_t) dig_P9) * ((int32_t) (((p >> 3) * (p >> 3)) >> 13))) >> 12;
-    var2 = (((int32_t) (p >> 2)) * ((int32_t) dig_P8)) >> 13;
-    p = (uint32_t) ((int32_t) p + ((var1 + var2 + dig_P7) >> 4));
-
-    return p;
-}
-
-uint32_t compensate_humidity(int32_t adc_H) {
-    int32_t v_x1_u32r;
-    v_x1_u32r = (t_fine - ((int32_t) 76800));
-    v_x1_u32r = (((((adc_H << 14) - (((int32_t) dig_H4) << 20) - (((int32_t) dig_H5) * v_x1_u32r)) +
-                   ((int32_t) 16384)) >> 15) * (((((((v_x1_u32r * ((int32_t) dig_H6)) >> 10) * (((v_x1_u32r *
-                                                                                                  ((int32_t) dig_H3))
-            >> 11) + ((int32_t) 32768))) >> 10) + ((int32_t) 2097152)) *
-                                                 ((int32_t) dig_H2) + 8192) >> 14));
-    v_x1_u32r = (v_x1_u32r - (((((v_x1_u32r >> 15) * (v_x1_u32r >> 15)) >> 7) * ((int32_t) dig_H1)) >> 4));
-    v_x1_u32r = (v_x1_u32r < 0 ? 0 : v_x1_u32r);
-    v_x1_u32r = (v_x1_u32r > 419430400 ? 419430400 : v_x1_u32r);
-
-    return (uint32_t) (v_x1_u32r >> 12);
-}
-
-#ifdef PICO_DEFAULT_SPI_CSN_PIN
 static inline void cs_select() {
     asm volatile("nop \n nop \n nop");
     gpio_put(PICO_DEFAULT_SPI_CSN_PIN, 0);  // Active low
@@ -119,9 +40,7 @@ static inline void cs_deselect() {
     gpio_put(PICO_DEFAULT_SPI_CSN_PIN, 1);
     asm volatile("nop \n nop \n nop");
 }
-#endif
 
-#if defined(spi_default) && defined(PICO_DEFAULT_SPI_CSN_PIN)
 static void write_register(uint8_t reg, uint8_t data) {
     uint8_t buf[2];
     buf[0] = reg & 0x7f;  // remove read bit as this is a write
@@ -145,57 +64,28 @@ static void read_registers(uint8_t reg, uint8_t *buf, uint16_t len) {
     sleep_ms(10);
 }
 
-/* This function reads the manufacturing assigned compensation parameters from the device */
-void read_compensation_parameters() {
-    uint8_t buffer[26];
-
-    read_registers(0x88, buffer, 24);
-
-    dig_T1 = buffer[0] | (buffer[1] << 8);
-    dig_T2 = buffer[2] | (buffer[3] << 8);
-    dig_T3 = buffer[4] | (buffer[5] << 8);
-
-    dig_P1 = buffer[6] | (buffer[7] << 8);
-    dig_P2 = buffer[8] | (buffer[9] << 8);
-    dig_P3 = buffer[10] | (buffer[11] << 8);
-    dig_P4 = buffer[12] | (buffer[13] << 8);
-    dig_P5 = buffer[14] | (buffer[15] << 8);
-    dig_P6 = buffer[16] | (buffer[17] << 8);
-    dig_P7 = buffer[18] | (buffer[19] << 8);
-    dig_P8 = buffer[20] | (buffer[21] << 8);
-    dig_P9 = buffer[22] | (buffer[23] << 8);
-
-    dig_H1 = buffer[25];
-
-    read_registers(0xE1, buffer, 8);
-
-    dig_H2 = buffer[0] | (buffer[1] << 8);
-    dig_H3 = (int8_t) buffer[2];
-    dig_H4 = buffer[3] << 4 | (buffer[4] & 0xf);
-    dig_H5 = (buffer[5] >> 4) | (buffer[6] << 4);
-    dig_H6 = (int8_t) buffer[7];
+int32_t platform_write(void *handle, uint8_t Reg, const uint8_t *Bufp, uint16_t len){
+    cs_select();
+    spi_write_blocking(spi_default, &Reg, 1);
+    spi_write_blocking(spi_default, Bufp, len);
+    cs_deselect();
+    sleep_ms(10);
+    return 0;
 }
-
-static void bme280_read_raw(int32_t *humidity, int32_t *pressure, int32_t *temperature) {
-    uint8_t buffer[8];
-
-    read_registers(0xF7, buffer, 8);
-    *pressure = ((uint32_t) buffer[0] << 12) | ((uint32_t) buffer[1] << 4) | (buffer[2] >> 4);
-    *temperature = ((uint32_t) buffer[3] << 12) | ((uint32_t) buffer[4] << 4) | (buffer[5] >> 4);
-    *humidity = (uint32_t) buffer[6] << 8 | buffer[7];
+int32_t platform_read(void *handle, uint8_t Reg, uint8_t *Bufp, uint16_t len){
+    cs_select();
+    spi_write_blocking(spi_default, &Reg, 1);
+    spi_read_blocking(spi_default, 0, Bufp, len);
+    cs_deselect();
+    return 0;
 }
-#endif
+void platform_delay(uint32_t millisec){
+    busy_wait_ms(millisec);
+}
 
 int main() {
     stdio_init_all();
-#if !defined(spi_default) || !defined(PICO_DEFAULT_SPI_SCK_PIN) || !defined(PICO_DEFAULT_SPI_TX_PIN) || !defined(PICO_DEFAULT_SPI_RX_PIN) || !defined(PICO_DEFAULT_SPI_CSN_PIN)
-#warning spi/bme280_spi example requires a board with SPI pins
-    puts("Default SPI pins were not defined");
-#else
 
-    printf("Hello, bme280! Reading raw data from registers via SPI...\n");
-
-    // This example will use SPI0 at 0.5MHz.
     spi_init(spi_default, 500 * 1000);
     gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
     gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
@@ -210,32 +100,134 @@ int main() {
     // Make the CS pin available to picotool
     bi_decl(bi_1pin_with_name(PICO_DEFAULT_SPI_CSN_PIN, "SPI CS"));
 
-    // See if SPI is working - interrograte the device for its I2C ID number, should be 0x60
-    uint8_t id;
-    read_registers(0xD0, &id, 1);
-    printf("Chip ID is 0x%x\n", id);
+    lis2dh12_reg_t reg;
+    stmdev_ctx_t dev_ctx; /** xxxxxxx is the used part number **/
+    uint8_t i, j;
+    dev_ctx.write_reg = platform_write;
+    dev_ctx.read_reg = platform_read;
+    dev_ctx.mdelay = platform_delay;
 
-    read_compensation_parameters();
+    /* Wait sensor boot time */
+    platform_delay(BOOT_TIME);
+    /* Check device ID */
+    lis2dh12_device_id_get(&dev_ctx, &reg.byte);
 
-    write_register(0xF2, 0x1); // Humidity oversampling register - going for x1
-    write_register(0xF4, 0x27);// Set rest of oversampling modes and run mode to normal
-
-    int32_t humidity, pressure, temperature;
-
-    while (1) {
-        bme280_read_raw(&humidity, &pressure, &temperature);
-
-        // These are the raw numbers from the chip, so we need to run through the
-        // compensations to get human understandable numbers
-        pressure = compensate_pressure(pressure);
-        temperature = compensate_temp(temperature);
-        humidity = compensate_humidity(humidity);
-
-        printf("Humidity = %.2f%%\n", humidity / 1024.0);
-        printf("Pressure = %dPa\n", pressure);
-        printf("Temp. = %.2fC\n", temperature / 100.0);
-
-        sleep_ms(1000);
+    if (reg.byte = LIS2DH12_ID)
+    {
+        printf("LIS2DH12 detected\n");
+    }else {
+        printf("LIS2DH12 not found!\n");
+        while (1);
     }
-#endif
+
+    /* Enable Block Data Update. */
+    lis2dh12_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
+    /* Set full scale to 2g. */
+    lis2dh12_full_scale_set(&dev_ctx, LIS2DH12_2g);
+    /* Set device in normal mode. */
+    lis2dh12_operating_mode_set(&dev_ctx, LIS2DH12_NM_10bit);
+    /* Set Output Data Rate to 1Hz. */
+    lis2dh12_data_rate_set(&dev_ctx, LIS2DH12_ODR_50Hz);
+    /* Wait stable output */
+    platform_delay(90);
+
+    /* Check if new value available */
+    do {
+    lis2dh12_status_get(&dev_ctx, &reg.status_reg);
+    } while (!reg.status_reg.zyxda);
+
+    /* Read dummy data and discard it */
+    lis2dh12_acceleration_raw_get(&dev_ctx, data_raw_acceleration);
+    /* Read 5 sample and get the average vale for each axis */
+    memset(acceleration_mg, 0x00, 3 * sizeof(float));
+
+    for (i = 0; i < 5; i++) {
+    /* Check if new value available */
+    do {
+        lis2dh12_status_get(&dev_ctx, &reg.status_reg);
+    } while (!reg.status_reg.zyxda);
+
+    /* Read data and accumulate the mg value */
+    lis2dh12_acceleration_raw_get(&dev_ctx, data_raw_acceleration);
+
+    for (j = 0; j < 3; j++) {
+        acceleration_mg[j] += lis2dh12_from_fs2_nm_to_mg(
+                                data_raw_acceleration[j]);
+    }
+    }
+
+    /* Calculate the mg average values */
+    for (i = 0; i < 3; i++) {
+    acceleration_mg[i] /= 5.0f;
+    }
+
+    /* Enable Self Test positive (or negative) */
+    lis2dh12_self_test_set(&dev_ctx, LIS2DH12_ST_POSITIVE);
+    //lis2dh12_self_test_set(&dev_ctx, LIS2DH12_ST_NEGATIVE);
+    /* Wait stable output */
+    platform_delay(90);
+
+    /* Check if new value available */
+    do {
+    lis2dh12_status_get(&dev_ctx, &reg.status_reg);
+    } while (!reg.status_reg.zyxda);
+
+    /* Read dummy data and discard it */
+    lis2dh12_acceleration_raw_get(&dev_ctx, data_raw_acceleration);
+    /* Read 5 sample and get the average vale for each axis */
+    memset(acceleration_st_mg, 0x00, 3 * sizeof(float));
+
+    for (i = 0; i < 5; i++) {
+    /* Check if new value available */
+    do {
+        lis2dh12_status_get(&dev_ctx, &reg.status_reg);
+    } while (!reg.status_reg.zyxda);
+
+    /* Read data and accumulate the mg value */
+    lis2dh12_acceleration_raw_get(&dev_ctx, data_raw_acceleration);
+
+    for (j = 0; j < 3; j++) {
+        acceleration_st_mg[j] += lis2dh12_from_fs2_nm_to_mg(
+                                    data_raw_acceleration[j]);
+    }
+    }
+
+    /* Calculate the mg average values */
+    for (i = 0; i < 3; i++) {
+    acceleration_st_mg[i] /= 5.0f;
+    }
+
+    /* Calculate the mg values for self test */
+    for (i = 0; i < 3; i++) {
+    test_val_mg[i] = fabs((acceleration_st_mg[i] - acceleration_mg[i]));
+    }
+
+    min_st_limit_mg = lis2dh12_from_fs2_nm_to_mg(MIN_ST_LIMIT_LSb);
+    max_st_limit_mg = lis2dh12_from_fs2_nm_to_mg(MAX_ST_LIMIT_LSb);
+
+    /* Check self test limit */
+    for (i = 0; i < 3; i++) {
+    if (( min_st_limit_mg < test_val_mg[i] ) &&
+        ( test_val_mg[i] < max_st_limit_mg)) {
+        printf("Axis[%d]: lmt min %4.2f mg - lmt max %4.2f mg - val %4.2f mg - PASS\r\n",
+                i, min_st_limit_mg, max_st_limit_mg, test_val_mg[i]);
+    }
+
+    else {
+        printf("Axis[%d]: lmt min %4.2f mg - lmt max %4.2f mg - val %4.2f mg - FAIL\r\n",
+                i, min_st_limit_mg, max_st_limit_mg, test_val_mg[i]);
+    }
+
+    // tx_com(tx_buffer, strlen((char const *)tx_buffer));
+    }
+
+    /* Disable Self Test */
+    lis2dh12_self_test_set(&dev_ctx, LIS2DH12_ST_DISABLE);
+    /* Disable sensor. */
+    lis2dh12_data_rate_set(&dev_ctx, LIS2DH12_POWER_DOWN);
+
+    // while (1) {
+
+    //     sleep_ms(1000);
+    // }
 }
