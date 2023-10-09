8a78a7323fb7f7212df8a9ac6c1ddb1fce8be8c4
Andrey Basov <dev.basov@gmail.com>
2023-10-09 02:11:44 +0100

    lis2dh12 demo

--- main.c
+++ main.c
@@ -1,9 +1,3 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 #include <stdio.h>
 #include <string.h>
 
@@ -16,6 +10,9 @@
 #define SPI_MISO 20
 #define SPI_CLK 18
 #define SPI_CS 21
+#define INT1 17
+#define INT2 16
+#define LED 25
 
 #define READ_BIT 0x80
 
@@ -33,6 +30,8 @@ static uint8_t tx_buffer[1000];
 static float test_val_mg[3];
 static float max_st_limit_mg;
 static float min_st_limit_mg;
+
+stmdev_ctx_t dev_ctx; /** xxxxxxx is the used part number **/
 
 static inline void cs_select() {
   gpio_put(SPI_CS, 0);  // Active low
@@ -62,6 +61,24 @@ int32_t platform_read(void *handle, uint8_t Reg, uint8_t *Bufp, uint16_t len) {
 }
 void platform_delay(uint32_t millisec) { busy_wait_ms(millisec); }
 
+void gpio_callback(uint gpio, uint32_t events) {
+  static int int_count = 0;
+  if (gpio == INT1 && events == GPIO_IRQ_EDGE_RISE) {
+    gpio_put(LED, !gpio_get(LED));
+    int16_t data_raw_acceleration[3];
+    lis2dh12_acceleration_raw_get(&dev_ctx, data_raw_acceleration);
+  }
+  if (gpio == INT2) {
+    printf("INT2 :%d ", int_count);
+    if (events == GPIO_IRQ_EDGE_FALL) {
+      printf("sleep->wake\n");
+    } else if (events == GPIO_IRQ_EDGE_RISE) {
+      printf("wake->sleep\n");
+    }
+    int_count++;
+  }
+}
+
 int main() {
   stdio_init_all();
   printf("LIS2DH12 demo\n");
@@ -75,19 +92,29 @@ int main() {
   gpio_set_function(SPI_CLK, GPIO_FUNC_SPI);
   gpio_set_function(SPI_MOSI, GPIO_FUNC_SPI);
   gpio_pull_up(SPI_CS);
-  // Make the SPI pins available to picotool
-  // bi_decl(bi_3pins_with_func(PICO_DEFAULT_SPI_RX_PIN,
-  // PICO_DEFAULT_SPI_TX_PIN, PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI));
+
+  gpio_init(LED);
+  gpio_set_dir(LED, GPIO_OUT);
+  gpio_put(LED, 0);
 
   // Chip select is active-low, so we'll initialise it to a driven-high state
   gpio_init(SPI_CS);
   gpio_set_dir(SPI_CS, GPIO_OUT);
   gpio_put(SPI_CS, 1);
-  // Make the CS pin available to picotool
-  // bi_decl(bi_1pin_with_name(PICO_DEFAULT_SPI_CSN_PIN, "SPI CS"));
+
+  gpio_init(INT1);
+  gpio_set_dir(INT1, GPIO_IN);
+  gpio_pull_up(INT1);
+  gpio_set_irq_enabled_with_callback(INT1, GPIO_IRQ_EDGE_RISE, true,
+                                     &gpio_callback);
+
+  gpio_init(INT2);
+  gpio_set_dir(INT2, GPIO_IN);
+  gpio_pull_up(INT2);
+  gpio_set_irq_enabled_with_callback(
+      INT2, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
 
   lis2dh12_reg_t reg;
-  stmdev_ctx_t dev_ctx; /** xxxxxxx is the used part number **/
   uint8_t i, j;
   dev_ctx.write_reg = platform_write;
   dev_ctx.read_reg = platform_read;
@@ -104,6 +131,7 @@ int main() {
   } else {
     printf("LIS2DH12 not found!\n");
   }
+
   /* Enable Block Data Update. */
   lis2dh12_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
   /* Set full scale to 2g. */
@@ -112,103 +140,39 @@ int main() {
   lis2dh12_operating_mode_set(&dev_ctx, LIS2DH12_NM_10bit);
   /* Set Output Data Rate to 1Hz. */
   lis2dh12_data_rate_set(&dev_ctx, LIS2DH12_ODR_50Hz);
-  /* Wait stable output */
-  platform_delay(90);
 
-  /* Check if new value available */
-  do {
-    lis2dh12_status_get(&dev_ctx, &reg.status_reg);
-  } while (!reg.status_reg.zyxda);
-
-  /* Read dummy data and discard it */
+  lis2dh12_ctrl_reg3_t ctrl_reg3;
+  ctrl_reg3.i1_zyxda = 1;
+  lis2dh12_pin_int1_config_set(&dev_ctx, &ctrl_reg3);
+  int16_t data_raw_acceleration[3];
   lis2dh12_acceleration_raw_get(&dev_ctx, data_raw_acceleration);
-  /* Read 5 sample and get the average vale for each axis */
-  memset(acceleration_mg, 0x00, 3 * sizeof(float));
 
-  for (i = 0; i < 5; i++) {
-    /* Check if new value available */
-    do {
-      lis2dh12_status_get(&dev_ctx, &reg.status_reg);
-    } while (!reg.status_reg.zyxda);
+  // lis2dh12_int1_cfg_t int1_cfg;
+  // int1_cfg.aoi = 0;
+  // int1_cfg._6d = 0;
+  // int1_cfg.xhie = 1;
+  // int1_cfg.xlie = 0;
+  // int1_cfg.yhie = 1;
+  // int1_cfg.ylie = 0;
+  // int1_cfg.zhie = 1;
+  // int1_cfg.zlie = 0;
+  // lis2dh12_int1_gen_conf_set(&dev_ctx, &int1_cfg);
+  // lis2dh12_int1_gen_threshold_set(&dev_ctx, 50);
+  // lis2dh12_int1_gen_duration_set(&dev_ctx, 1);
+  // lis2dh12_ctrl_reg6_t ctrl_reg6;
+  // ctrl_reg6.i2_ia1 = 1;
+  // lis2dh12_pin_int2_config_set(&dev_ctx, &ctrl_reg6);
+  lis2dh12_hp_t high_pass = LIS2DH12_DISC_FROM_INT_GENERATOR;
+  lis2dh12_high_pass_int_conf_set(&dev_ctx, high_pass);
 
-    /* Read data and accumulate the mg value */
-    lis2dh12_acceleration_raw_get(&dev_ctx, data_raw_acceleration);
+  lis2dh12_ctrl_reg6_t ctrl_reg6;
+  ctrl_reg6.i2_act = 1;
+  ctrl_reg6.int_polarity = 0;
+  lis2dh12_pin_int2_config_set(&dev_ctx, &ctrl_reg6);
+  lis2dh12_act_threshold_set(&dev_ctx, 10);
+  lis2dh12_act_timeout_set(&dev_ctx, 10);
 
-    for (j = 0; j < 3; j++) {
-      acceleration_mg[j] +=
-          lis2dh12_from_fs2_nm_to_mg(data_raw_acceleration[j]);
-    }
+  while (1) {
+    tight_loop_contents();
   }
-
-  /* Calculate the mg average values */
-  for (i = 0; i < 3; i++) {
-    acceleration_mg[i] /= 5.0f;
-  }
-
-  /* Enable Self Test positive (or negative) */
-  lis2dh12_self_test_set(&dev_ctx, LIS2DH12_ST_POSITIVE);
-  // lis2dh12_self_test_set(&dev_ctx, LIS2DH12_ST_NEGATIVE);
-  /* Wait stable output */
-  platform_delay(90);
-
-  /* Check if new value available */
-  do {
-    lis2dh12_status_get(&dev_ctx, &reg.status_reg);
-  } while (!reg.status_reg.zyxda);
-
-  /* Read dummy data and discard it */
-  lis2dh12_acceleration_raw_get(&dev_ctx, data_raw_acceleration);
-  /* Read 5 sample and get the average vale for each axis */
-  memset(acceleration_st_mg, 0x00, 3 * sizeof(float));
-
-  for (i = 0; i < 5; i++) {
-    /* Check if new value available */
-    do {
-      lis2dh12_status_get(&dev_ctx, &reg.status_reg);
-    } while (!reg.status_reg.zyxda);
-
-    /* Read data and accumulate the mg value */
-    lis2dh12_acceleration_raw_get(&dev_ctx, data_raw_acceleration);
-
-    for (j = 0; j < 3; j++) {
-      acceleration_st_mg[j] +=
-          lis2dh12_from_fs2_nm_to_mg(data_raw_acceleration[j]);
-    }
-  }
-
-  /* Calculate the mg average values */
-  for (i = 0; i < 3; i++) {
-    acceleration_st_mg[i] /= 5.0f;
-  }
-
-  /* Calculate the mg values for self test */
-  for (i = 0; i < 3; i++) {
-    test_val_mg[i] = fabs((acceleration_st_mg[i] - acceleration_mg[i]));
-  }
-
-  min_st_limit_mg = lis2dh12_from_fs2_nm_to_mg(MIN_ST_LIMIT_LSb);
-  max_st_limit_mg = lis2dh12_from_fs2_nm_to_mg(MAX_ST_LIMIT_LSb);
-
-  /* Check self test limit */
-  for (i = 0; i < 3; i++) {
-    if ((min_st_limit_mg < test_val_mg[i]) &&
-        (test_val_mg[i] < max_st_limit_mg)) {
-      printf(
-          "Axis[%d]: lmt min %4.2f mg - lmt max %4.2f mg - val %4.2f mg - "
-          "PASS\r\n",
-          i, min_st_limit_mg, max_st_limit_mg, test_val_mg[i]);
-    }
-
-    else {
-      printf(
-          "Axis[%d]: lmt min %4.2f mg - lmt max %4.2f mg - val %4.2f mg - "
-          "FAIL\r\n",
-          i, min_st_limit_mg, max_st_limit_mg, test_val_mg[i]);
-    }
-  }
-
-  /* Disable Self Test */
-  lis2dh12_self_test_set(&dev_ctx, LIS2DH12_ST_DISABLE);
-  /* Disable sensor. */
-  lis2dh12_data_rate_set(&dev_ctx, LIS2DH12_POWER_DOWN);
 }
