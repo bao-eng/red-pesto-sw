1a37238d43b0b311c9d2bd03493898527d33ba02
Andrey Basov <dev.basov@gmail.com>
2023-10-29 03:03:24 +0400

    move gpio init to separate function

--- src/lis2dh12.h
+++ src/lis2dh12.h
@@ -40,35 +40,7 @@ int32_t platform_read(void *handle, uint8_t Reg, uint8_t *Bufp, uint16_t len) {
 }
 void platform_delay(uint32_t millisec) { busy_wait_ms(millisec); }
 
-void red_pesto_lis2dh12_init(gpio_irq_callback_t callback){
-  spi_init(spi0, 500 * 1000);
-  spi_set_format(spi0,  // SPI instance
-                 8,     // Number of bits per transfer
-                 1,     // Polarity (CPOL)
-                 1,     // Phase (CPHA)
-                 SPI_MSB_FIRST);
-
-  gpio_set_function(SPI_MISO, GPIO_FUNC_SPI);
-  gpio_set_function(SPI_CLK, GPIO_FUNC_SPI);
-  gpio_set_function(SPI_MOSI, GPIO_FUNC_SPI);
-  gpio_pull_up(SPI_CS);
-
-  gpio_init(SPI_CS);
-  gpio_set_dir(SPI_CS, GPIO_OUT);
-  gpio_put(SPI_CS, 1);
-
-  gpio_init(INT1);
-  gpio_set_dir(INT1, GPIO_IN);
-  gpio_pull_up(INT1);
-  gpio_set_irq_enabled_with_callback(INT1, GPIO_IRQ_EDGE_RISE, true,
-                                     callback);
-
-  gpio_init(INT2);
-  gpio_set_dir(INT2, GPIO_IN);
-  gpio_pull_up(INT2);
-  gpio_set_irq_enabled_with_callback(
-      INT2, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, callback);
-
+void red_pesto_lis2dh12_init(){
   lis2dh12_reg_t reg;
   dev_ctx.write_reg = platform_write;
   dev_ctx.read_reg = platform_read;
