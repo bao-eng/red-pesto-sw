4d69fffbdd8251e3811cc0bd05aeb15d815225da
Andrey Basov <dev.basov@gmail.com>
2023-10-29 22:40:35 +0400

    i2c init function

---
+++ src/i2c.h
@@ -0,0 +1,12 @@
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+
+#include "board.h"
+
+void red_pesto_i2c_init(){
+  i2c_init(i2c0, 100 * 1000);
+  gpio_set_function(I2C0_SDA_PIN, GPIO_FUNC_I2C);
+  gpio_set_function(I2C0_SCL_PIN, GPIO_FUNC_I2C);
+  gpio_pull_up(I2C0_SDA_PIN);
+  gpio_pull_up(I2C0_SCL_PIN);
+}
