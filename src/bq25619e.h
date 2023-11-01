34c1073eda200d803bde4b20897471f10a379de1
Andrey Basov <dev.basov@gmail.com>
2023-11-02 01:59:14 +0400

    bq25619e interrupt handling fixed

--- src/bq25619e.h
+++ src/bq25619e.h
@@ -19,29 +19,53 @@ struct bq256xx_device bq25619e = {
 void red_pesto_bq25619e_init(){
   bq256xx_charger_reset(&bq25619e);
   bq256xx_hw_init(&bq25619e);
+  bq256xx_get_state(&bq25619e, &bq25619e.state);
 }
 
 void bq_int_handler(){
-  busy_wait_ms(100);  // wait for the CC voltage to set
-  const float conversion_factor = 3.3f / (1 << 12);
-  adc_select_input(CC1_ADC);
-  float cc1 = adc_read() * conversion_factor;
-  adc_select_input(CC2_ADC);
-  float cc2 = adc_read() * conversion_factor;
-  float cc = cc1 > cc2 ? cc1 : cc2;
-  if ((cc > VRD_USB_MIN) && (cc < VRD_USB_MAX)) {
-    DEBUG_PRINT("vRd-USB (500mA)");
-    bq256xx_set_input_curr_lim(&bq25619e, 500000);
-  } else if ((cc > VRD_15_MIN) && (cc < VRD_15_MAX)) {
-    DEBUG_PRINT("vRd-1.5");
-    bq256xx_set_input_curr_lim(&bq25619e, 1500000);
-  } else if ((cc > VRD_30_MIN) && (cc < VRD_30_MAX)) {
-    DEBUG_PRINT("vRd-3.0");
-    bq256xx_set_input_curr_lim(&bq25619e, 3000000);
-  } else if (cc < VRA_MAX) {
-    DEBUG_PRINT("vRa (disconnected)");
-    bq256xx_set_input_curr_lim(&bq25619e, 500000);
+  struct bq256xx_state tmp_state;
+  bq256xx_get_state(&bq25619e, &tmp_state);
+  if(tmp_state.online){
+    if(!bq25619e.state.online){
+      DEBUG_PRINT("Charging cable connected");
+      const float conversion_factor =  3.3f / (1 << 12);
+      adc_select_input(CC1_ADC);
+      float cc1 = adc_read() * conversion_factor;
+      adc_select_input(CC2_ADC);
+      float cc2 = adc_read() * conversion_factor;
+      float cc = cc1 > cc2 ? cc1 : cc2;
+      if ((cc > VRD_USB_MIN) && (cc < VRD_USB_MAX)) {
+        DEBUG_PRINT("vRd-USB (500mA)");
+        bq256xx_set_input_curr_lim(&bq25619e, 500000);
+      } else if ((cc > VRD_15_MIN) && (cc < VRD_15_MAX)) {
+        DEBUG_PRINT("vRd-1.5");
+        bq256xx_set_input_curr_lim(&bq25619e, 1500000);
+      } else if ((cc > VRD_30_MIN) && (cc < VRD_30_MAX)) {
+        DEBUG_PRINT("vRd-3.0");
+        bq256xx_set_input_curr_lim(&bq25619e, 3000000);
+      }
+    }
+    switch (tmp_state.chrg_stat)
+    {
+      case BQ256XX_CHRG_STAT_NOT_CHRGING:
+        DEBUG_PRINT("Not charging");
+        break;
+      case BQ256XX_CHRG_STAT_PRECHRGING:
+        DEBUG_PRINT("Precharging");
+        break;
+      case BQ256XX_CHRG_STAT_FAST_CHRGING:
+        DEBUG_PRINT("Fast charging");
+        break;
+      case BQ256XX_CHRG_STAT_CHRG_TERM:
+        DEBUG_PRINT("Charging termination");
+        break;
+    }
+  }else{
+    if(bq25619e.state.online){
+      DEBUG_PRINT("Charging cable disconnected");
+    }
   }
+  bq25619e.state = tmp_state;
 }
 
 void bq_enter_ship_mode(){
