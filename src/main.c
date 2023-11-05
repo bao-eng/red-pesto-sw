c92513f91341344b4887b8875beba0c23d37a6ca
Andrey Basov <dev.basov@gmail.com>
2023-11-05 20:23:23 +0400

    LIS2DH accelerometer integrated

--- src/main.c
+++ src/main.c
@@ -16,6 +16,7 @@
 #define LED_RED_NODE DT_ALIAS(led_red)
 #define VEML7700_LEFT_NODE DT_ALIAS(veml7700_left)
 #define VEML7700_RIGHT_NODE DT_ALIAS(veml7700_right)
+#define LIS2DH_NODE DT_NODELABEL(lis2dh)
 
 /*
  * A build error on this line means your board is unsupported.
@@ -25,6 +26,7 @@ static const struct gpio_dt_spec led_green = GPIO_DT_SPEC_GET(LED_GREEN_NODE, gp
 static const struct gpio_dt_spec led_red = GPIO_DT_SPEC_GET(LED_RED_NODE, gpios);
 const struct device *const veml7700_left = DEVICE_DT_GET(VEML7700_LEFT_NODE);
 const struct device *const veml7700_right = DEVICE_DT_GET(VEML7700_RIGHT_NODE);
+const struct device *const lis2dh = DEVICE_DT_GET(LIS2DH_NODE);
 
 int main(void)
 {
@@ -49,6 +51,7 @@ int main(void)
 	}
 
 	struct sensor_value val;
+	struct sensor_value accel[3];
 	while (1) {
 		ret = sensor_sample_fetch(veml7700_left);
 		ret = sensor_channel_get(veml7700_left, SENSOR_CHAN_LIGHT, &val);
@@ -56,6 +59,11 @@ int main(void)
 		ret = sensor_sample_fetch(veml7700_right);
 		ret = sensor_channel_get(veml7700_right, SENSOR_CHAN_LIGHT, &val);
 		printk("VEML770-Right: %d\n", val.val1);
+		ret = sensor_sample_fetch(lis2dh);
+		ret = sensor_channel_get(lis2dh, SENSOR_CHAN_ACCEL_XYZ, accel);
+		printk("LIS2DH: %.2f %.2f %.2f\n", accel[0].val1 + accel[0].val2*0.000001,
+			accel[1].val1 + accel[1].val2*0.000001,
+			accel[2].val1 + accel[2].val2*0.000001);
 		ret = gpio_pin_toggle_dt(&led_green);
 		ret = gpio_pin_toggle_dt(&led_red);
 		if (ret < 0) {
